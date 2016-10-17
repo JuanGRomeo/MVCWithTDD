3b799352475307532d38e8b1a7e57a6f1e9060c0
juan.romeo <juan.romeo.monreal@gmail.com>
2016-10-17 23:36:30 +0200

    Post repository added

---
+++ MVCWithTDD.Data/Repository/Interfaces/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using EnjoyMyTown.Data.Infraestructure.Interfaces;
+using MVCWithTDD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCWithTDD.Data.Repository.Interfaces
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+}
---
+++ MVCWithTDD.Data/Repository/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using EnjoyMyTown.Data.Infraestructure;
+using EnjoyMyTown.Data.Infraestructure.Interfaces;
+using MVCWithTDD.Data.Repository.Interfaces;
+using MVCWithTDD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithTDD.Data.Repository
+{
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+            {
+        }
+    }
+}
