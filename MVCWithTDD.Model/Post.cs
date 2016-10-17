ff087ede4c6a5be6f42d62018a3164326decf268
juan.romeo <juan.romeo.monreal@gmail.com>
2016-10-17 23:38:40 +0200

    Set identity to IdPost and apply immutability

--- MVCWithTDD.Model/Post.cs
+++ MVCWithTDD.Model/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,8 @@ namespace MVCWithTDD.Model
 {
     public class Post
     {
-        public int IdPost { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPost { get; protected set; }
         public string Title { get; set; }
         public string Content { get; set; }
     }
