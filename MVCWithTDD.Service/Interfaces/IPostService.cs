17d5333cd82dfcfcebbe63bcdb17d8114e1f7a16
juan.romeo <juan.romeo.monreal@gmail.com>
2016-10-18 00:18:33 +0200

    Service pattern and tests added

---
+++ MVCWithTDD.Service/Interfaces/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using MVCWithTDD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCWithTDD.Service.Interfaces
+{
+    public interface IPostService
+    {
+        void Create(Post post);
+        List<Post> ListRecentPosts(int retrievalCount);
+    }
+}
---
+++ MVCWithTDD.Service/PostService.cs
@@ -0,0 +1,36 @@
+﻿using MVCWithTDD.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVCWithTDD.Model;
+using MVCWithTDD.Data.Repository.Interfaces;
+using EnjoyMyTown.Data.Infraestructure.Interfaces;
+
+namespace MVCWithTDD.Service
+{
+    public class PostService : IPostService
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IPostRepository postRepository;
+
+        public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            this.postRepository = postRepository;
+        }
+
+        public void Create(Post post)
+        {
+            this.postRepository.Add(post);
+            this.unitOfWork.Commit();
+        }
+
+        public List<Post> ListRecentPosts(int retrievalCount)
+        {
+            return this.postRepository.GetAll().OrderByDescending(post => post.IdPost).Take(retrievalCount).ToList();
+        }
+    }
+}
